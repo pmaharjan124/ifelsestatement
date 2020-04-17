34a3314181bddfa29f749febd8e47f2df999bf79
pmaharjan124 <maharjanpuja@gmail.com>
2020-04-17 15:06:49 -0400

    first commit

---
+++ src/IfElseStatement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class IfElseStatement {
+
+
+    public static void main(String[] args) {
+        int number = 10;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextLine();
+
+        if (number > 0) {
+            System.out.println("Number is positive.");
+        }
+        if else(number < 0) {
+            System.out.println("Number is not positive.");
+        }
+         else{
+            System.out.println("Application finished.");
+        }
+    }
+}
